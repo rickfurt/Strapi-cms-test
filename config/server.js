edc5319ccb202696993c26f2fa5253791c1a508c
Ricardo <ricardofurtado2609@gmail.com>
2022-07-22 21:48:30 +1000

    initial

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'cd9142b53c6640cfacc34b29d7ddf098'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '5af50b74-f07c-44c5-aa61-789ed71384f4'
+};
